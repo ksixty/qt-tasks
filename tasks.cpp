80a1d87346e1e67b345f70ed2f47dbd129eeda5c
Vanya Klimenko <v@vanyaklimenko.ru>
2019-10-02 20:20:34 +0300

    [v. 1.0] Fix everything
    Redesigned the UI layout for it to be more compact.
    Fixed indexing errors in current_tasks.
    Set up keyboard shortcuts.
    Auto-save now works everywhere.
    Fixed accessibility and keyboard related mishaps.
    
    That’s pretty much it for the first (and, in this case, unfortunately
    last version of this application).

--- tasks.cpp
+++ tasks.cpp
@@ -26,6 +26,7 @@ Tasks::Tasks(QWidget *parent)
     , ui(new Ui::Tasks)
 {
     ui->setupUi(this);
+    current_file = "";
     update_view();
 }
 
@@ -46,8 +47,7 @@ void Tasks::read_current_file()
 
     current_tasks.clear();
     QTextStream current_ts(&file);
-    while (!current_ts.atEnd())
-    {
+    while (!current_ts.atEnd()) {
        QStringList item = current_ts.readLine().split(DELIMITER);
        if (item.size() == 3) {
            struct task task
@@ -63,13 +63,31 @@ void Tasks::read_current_file()
     file.close();
 }
 
+void Tasks::save_current_file()
+{
+    if (current_file.length() > 0) {
+       QFile file(current_file);
+       if (!file.open(QFile::ReadWrite | QFile::Truncate | QFile::Text)) {
+           alert("Could not save the file");
+           return ;
+       }
+       QTextStream current_ts(&file);
+       for (int i = 0; i < current_tasks.size(); i++) {
+           struct task task = current_tasks[i];
+           current_ts << task.title << DELIMITER
+                      << task.time << DELIMITER
+                      << (task.done ? "DONE" : "TODO") << endl;
+       }
+
+    }
+}
+
 void Tasks::update_view()
 {
     model = new QStringListModel(this);
     QStringList items;
 
-    for (int i = 0; i < current_tasks.size(); i++)
-    {
+    for (int i = 0; i < current_tasks.size(); i++) {
         QString time = QDateTime
                 ::fromSecsSinceEpoch(current_tasks[i].time)
                 .toString("yyyy-MM-dd HH:mm:ss");
@@ -80,17 +98,43 @@ void Tasks::update_view()
     }
     model->setStringList(items);
     ui->listView->setModel(model);
-    ui->label->setText(current_todo > 0
-                       ? QString::number(current_todo) + " to do"
-                       : "Tasks");
+    Tasks::setWindowTitle((current_file.length() > 0
+                           ? current_file
+                           : "Unsaved File") + " — " +
+                          (current_todo > 0
+                           ? QString::number(current_todo) + " To Do — Tasks"
+                           : "Tasks"));
+    ui->listView->setCurrentIndex(model->index(0, 0));
 }
 
 void Tasks::add_task(QString title)
 {
+    current_todo++;
     struct task task { title, QDateTime::currentSecsSinceEpoch(), false };
     current_tasks.prepend(task);
-    current_todo++;
     update_view();
+}
+
+void Tasks::remove_task(int id)
+{
+    if (current_tasks.size() > 0 && id > -1) {
+        if (!current_tasks[id].done) current_todo--;
+        current_tasks.remove(id);
+        update_view();
+    }
+}
+
+void Tasks::toggle_task(int id)
+{
+    if (current_tasks.size() > 0 && id > -1) {
+        current_tasks[id].done ^= 1;
+        if (current_tasks[id].done) {
+            current_todo--;
+        } else {
+            current_todo++;
+        }
+        update_view();
+    }
 }
 
 void Tasks::alert(QString text)
@@ -114,6 +158,7 @@ void Tasks::on_actionNew_triggered()
     QString filename = QFileDialog::getSaveFileName(this, tr("Save Tasks List"),
                                QDir::homePath() + "/list.txt", tr("Text Documents (*.txt)"));
     current_file = filename;
+    update_view();
 }
 
 void Tasks::on_pushButton_clicked()
@@ -121,7 +166,9 @@ void Tasks::on_pushButton_clicked()
     QString text = ui->lineEdit->text().simplified();
     if (text.length()) {
         add_task(text);
+        save_current_file();
     }
+    ui->lineEdit->setText("");
 }
 
 void Tasks::on_lineEdit_returnPressed()
@@ -129,6 +176,31 @@ void Tasks::on_lineEdit_returnPressed()
     QString text = ui->lineEdit->text().simplified();
     if (text.length()) {
         add_task(text);
+        save_current_file();
     }
+    ui->lineEdit->setText("");
+}
 
+void Tasks::on_removeButton_clicked()
+{
+    remove_task(ui->listView->currentIndex().row());
+    save_current_file();
+}
+
+void Tasks::on_doneButton_clicked()
+{
+    toggle_task(ui->listView->currentIndex().row());
+    save_current_file();
+}
+
+void Tasks::on_actionToggle_Status_triggered()
+{
+    toggle_task(ui->listView->currentIndex().row());
+    save_current_file();
+}
+
+void Tasks::on_actionRemove_Selected_Task_triggered()
+{
+    remove_task(ui->listView->currentIndex().row());
+    save_current_file();
 }
--- tasks.h
+++ tasks.h
@@ -22,10 +22,14 @@ public:
 private slots:
     void on_actionOpen_triggered();
     void on_actionNew_triggered();
-
     void on_pushButton_clicked();
-
     void on_lineEdit_returnPressed();
+    void on_removeButton_clicked();
+    void on_doneButton_clicked();
+
+    void on_actionToggle_Status_triggered();
+
+    void on_actionRemove_Selected_Task_triggered();
 
 private:
     Ui::Tasks *ui;
@@ -39,5 +43,8 @@ private:
     unsigned int current_todo;
 
     void add_task(QString title);
+    void remove_task(int id);
+    void toggle_task(int id);
+    void save_current_file();
 };
 #endif // TASKS_H
